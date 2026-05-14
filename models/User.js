4dfc65f42eed8dae79e35644f8af294d61d66091
varaprasadbandi777 <varaprasadbandi777@gmail.com>
2026-05-14 14:27:37 +0530

    Update User model to allow capitalized roles

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['admin', 'teacher', 'student', 'parent'],
+      enum: ['Admin', 'Teacher', 'Student', 'Parent'],
       default: 'student',
     },
   },
